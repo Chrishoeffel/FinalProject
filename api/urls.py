c847a622a7fa89aa72f6479213569f08378dcd22
Chris Ladd <codebyladd@gmail.com>
2021-11-19 12:35:41 -0700

    setup calendat

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.conf.urls import url
 from . import views
 
+app_name = 'api'
 urlpatterns = [
-    path('', views.home, name='home'),
+    url(r'^index/$', views.index, name='index'),
+    url(r'^calendar/$', views.CalendarView.as_view(), name='calendar'),
 ]
