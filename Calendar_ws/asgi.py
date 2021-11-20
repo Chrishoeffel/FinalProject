95fabb7b1fd2fb269b549176be4c058392e12755
Chris Ladd <codebyladd@gmail.com>
2021-11-20 02:42:26 -0700

    Final Calender found

--- Calendar_ws/asgi.py
+++ Calendar_ws/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Calender_ws.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Calendar_ws.settings')
 
 application = get_asgi_application()
