fd2df4abba7d435e8d50184057b395f71180109d
Chris Ladd <codebyladd@gmail.com>
2021-11-27 17:13:15 -0700

    added to allowed hosts

--- Calendar_ws/settings.py
+++ Calendar_ws/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '*', 'https://zencalendarapp.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', '*', 'zencalendarapp.herokuapp.com', '127.0.0.1']
 DATABASE_URL = env('DATABASE_URL')
 
 # Application definition
